942c9b069198118a88c51fb3e11bb967bdb235c2
Vaibhav Ghildiyal <vaibhavghildiyal2101@gmail.com>
2026-01-07 18:05:56 +0530

    feat: initialize FastAPI application with user and auth routers and database table creation on startup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from .lib.database import engine, Base
 
 # Iska matlab hai models wali file load karo taaki Base ko pata chale kitni tables hain
 from . import model
-from .routers import user  # 1. Apne naye router folder ko import karo
+from .routers import user, auth  # 1. Apne naye router folder ko import karo
 
 
 # 1. App ko initialize karo
 app = FastAPI(title="Linear Clone API")
-# 2. Ye line sabse important hai! 
+# 2. Ye line sabse important hai!
 # Isse saare signup routes main app se connect ho jayenge.
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 # 2. Ek test route banao (Browser pe dikhega)
